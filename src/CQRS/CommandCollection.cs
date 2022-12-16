69212fc5d36daaa4e00474dc77d128362dea139b
Nightowl <79215414+nightowl286@users.noreply.github.com>
2022-12-16 00:00:03 +0000

    Updated package and documented the implementations.

--- src/CQRS/CommandCollection.cs
+++ src/CQRS/CommandCollection.cs
@@ -5,12 +5,23 @@ using TNO.Dispatch;
 
 namespace TNO.CQRS
 {
+   /// <summary>
+   /// A dispatch collection for commands.
+   /// </summary>
    public class CommandCollection : DispatchCollection<ICommandRequest, ICommandCollection>, ICommandCollection
    {
+      #region Constructors
+      /// <summary>Creates a new command collection with the given <paramref name="scope"/>.</summary>
+      /// <inheritdoc/>
       public CommandCollection(IServiceScope scope) : base(scope) { }
+
+      /// <summary>Creates a new command collection with the given <paramref name="scope"/> and <paramref name="outerScope"/>.</summary>
+      /// <inheritdoc/>
       protected CommandCollection(IServiceFacade scope, DispatchCollection<ICommandRequest, ICommandCollection> outerScope) : base(scope, outerScope) { }
+      #endregion
 
       #region Methods
+      /// <inheritdoc/>
       public override ICommandCollection CreateScope() => new CommandCollection(_serviceFacade.CreateScope(), this);
       #endregion
    }
--- src/CQRS/QueryCollection.cs
+++ src/CQRS/QueryCollection.cs
@@ -5,12 +5,23 @@ using TNO.Dispatch;
 
 namespace TNO.CQRS
 {
+   /// <summary>
+   /// A dispatch collection for queries.
+   /// </summary>
    public class QueryCollection : DispatchCollection<IQueryRequest, IQueryCollection>, IQueryCollection
    {
+      #region Constructors
+      /// <summary>Creates a new query collection with the given <paramref name="scope"/>.</summary>
+      /// <inheritdoc/>
       public QueryCollection(IServiceScope scope) : base(scope) { }
+
+      /// <summary>Creates a new query collection with the given <paramref name="scope"/> and <paramref name="outerScope"/>.</summary>
+      /// <inheritdoc/>
       protected QueryCollection(IServiceFacade scope, DispatchCollection<IQueryRequest, IQueryCollection> outerScope) : base(scope, outerScope) { }
+      #endregion
 
       #region Methods
+      /// <inheritdoc/>
       public override IQueryCollection CreateScope() => new QueryCollection(_serviceFacade.CreateScope(), this);
       #endregion
    }
